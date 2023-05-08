18fb7d732e0ea18eb63cd44a19ab033cf2581fb0
Rajesh <Rajesh@gmail.com>
2023-05-08 14:21:50 +0530

    added new components for homepage

---
+++ raju.com/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+<Home></Home>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ raju.com/src/components/TopNav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './TopNav.css'
+const TopNav = () => {
+    return (
+        <div class='main-contain'>
+            <div id='logo'>LOGO</div>
+            <div id='NavBar'>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+
+            </div>
+        </div>
+        
+    )
+}
+
+export default TopNav
